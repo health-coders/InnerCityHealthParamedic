c9ae37455319917743ed72ecb8b7deb2ca2b5da4
Luis Alvarez <lealvarezp@unac.edu.co>
2020-05-27 01:40:49 -0500

    Configuración del inicio de Google (Testear)

---
+++ src/screens/ConsultasScreens/ConsultasRegistradasScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import TargetaConsulta from "../../components/targetaCosulta/TargetaConsulta";
+
+const ConsultasRegistradasScreen = ({navigation}) => {
+
+    const info = {
+        tipo: 'General',
+        fecha: '25/04/2021',
+        hora: '18:00',
+        direccion: 'Crra 85',
+        estado: false,
+        descripcion: 'Me siento mal',
+        nombrePaciente: 'Luis Alvarez',
+    }
+
+    return (
+        <>
+            <Text style={styles.title}>Consultas </Text>
+            <TargetaConsulta info={info}/>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    title: {
+        fontWeight: 'bold',
+        fontSize: 37,
+        marginVertical: 20
+    },
+});
+
+export default ConsultasRegistradasScreen;
